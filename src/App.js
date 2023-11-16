db0504a17ae2f89a7fef38dbb5af11b01472b976
saleek-pa <saleek.pa@gmail.com>
2023-11-16 20:48:52 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+// import { Route, Routes } from "react-router-dom";
+
+// import Header from "./Components/Header";
+import Home from "./Pages/Home";
+import "./Styles/style.css"
+
+function App() {
+   return (
+      <>
+         {/* <Header /> */}
+         <Home />
+
+         {/* <Header />
+
+        <Routes>
+          <Route path="/" exact component={Home} />
+          <Route path="/movie/:id" component={MovieDetailsPage} />
+        </Routes>
+
+        <Footer /> */}
+      </>
+   );
+}
+
+export default App;
---
+++ src/Pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../Components/Header";
+// import "../Styles/style.css"
+
+function Home() {
+   return (
+      <>
+         <Header />
+
+         
+      </>
+   );
+}
+
+export default Home;
