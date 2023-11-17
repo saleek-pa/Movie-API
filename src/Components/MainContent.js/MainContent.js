018ebd5082dc1dbc03b34ad4a0212cc91cb8b716
saleek-pa <saleek.pa@gmail.com>
2023-11-17 23:23:41 +0530

    Created Movie Card

--- src/Components/MainContent.js/MainContent.js
+++ src/Components/MainContent.js/MainContent.js
@@ -1,11 +1,72 @@
 import React from "react";
-import "./MainContent.css";
 import { Navbar } from "../Navbar/Navbar";
+import { movies } from "../../DummyData";
+import "./MainContent.css";
 
 export const MainContent = () => {
+   console.log(movies);
    return (
       <div className="main-content-container">
          <Navbar />
+         <div className="list-heading">
+            <h3>Trending Now</h3>
+         </div>
+         <div className="movie-card-container">
+            {movies.map((movie) => (
+               <div class="movie-card">
+                  <img src={movie.image} alt={movie.title} class="movie-image" />
+                  <div class="movie-title">
+                     {movie.title} ({movie.year})
+                  </div>
+                  <div class="movie-review">{movie.rating}</div>
+               </div>
+            ))}
+         </div>
+
+         <div className="list-heading">
+            <h3>Returning this Week</h3>
+         </div>
+         <div className="movie-card-container">
+            {movies.reverse().map((movie) => (
+               <div class="movie-card">
+                  <img src={movie.image} alt={movie.title} class="movie-image" />
+                  <div class="movie-title">
+                     {movie.title} ({movie.year})
+                  </div>
+                  <div class="movie-review">{movie.rating}</div>
+               </div>
+            ))}
+         </div>
+
+         <div className="list-heading">
+            <h3>New Shows Airing in November</h3>
+         </div>
+         <div className="movie-card-container">
+            {movies.reverse().map((movie) => (
+               <div class="movie-card">
+                  <img src={movie.image} alt={movie.title} class="movie-image" />
+                  <div class="movie-title">
+                     {movie.title} ({movie.year})
+                  </div>
+                  <div class="movie-review">{movie.rating}</div>
+               </div>
+            ))}
+         </div>
+
+         <div className="list-heading">
+            <h3>Most Popular</h3>
+         </div>
+         <div className="movie-card-container">
+            {movies.reverse().map((movie) => (
+               <div class="movie-card">
+                  <img src={movie.image} alt={movie.title} class="movie-image" />
+                  <div class="movie-title">
+                     {movie.title} ({movie.year})
+                  </div>
+                  <div class="movie-review">{movie.rating}</div>
+               </div>
+            ))}
+         </div>
       </div>
    );
 };
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -4,51 +4,67 @@ import "./Navbar.css";
 export const Navbar = () => {
    return (
       <div className="navbar">
-         <div class="search-input-container">
-            <input type="text" name="text" class="search-input" placeholder="search..." />
-            <span class="search-icon">
+         <div className="search-input-container">
+            <input type="text" name="text" className="search-input" placeholder="search..." />
+            <span className="search-icon">
                <svg width="19px" height="19px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-                  <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                  <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                  <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
                   <g id="SVGRepo_iconCarrier">
                      {" "}
                      <path
                         opacity="1"
                         d="M14 5H20"
                         stroke="#000"
-                        stroke-width="1.5"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
+                        strokeWidth="1.5"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
                      ></path>{" "}
                      <path
                         opacity="1"
                         d="M14 8H17"
                         stroke="#000"
-                        stroke-width="1.5"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
+                        strokeWidth="1.5"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
                      ></path>{" "}
                      <path
                         d="M21 11.5C21 16.75 16.75 21 11.5 21C6.25 21 2 16.75 2 11.5C2 6.25 6.25 2 11.5 2"
                         stroke="#000"
-                        stroke-width="2.5"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
+                        strokeWidth="2.5"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
                      ></path>{" "}
                      <path
                         opacity="1"
                         d="M22 22L20 20"
                         stroke="#000"
-                        stroke-width="3.5"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
+                        strokeWidth="3.5"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
                      ></path>{" "}
                   </g>
                </svg>
             </span>
          </div>
-         <div>
-            <button className="signin-btn">SIGN IN</button>
+         <div className="navbar-right-section">
+            <div>
+               <input id="checkbox_toggle" type="checkbox" className="check" />
+               <div className="checkbox">
+                  <label className="slide" htmlFor="checkbox_toggle">
+                     <label className="toggle" htmlFor="checkbox_toggle"></label>
+                     <label className="text" htmlFor="checkbox_toggle">
+                        Movie
+                     </label>
+                     <label className="text" htmlFor="checkbox_toggle">
+                        TV Show
+                     </label>
+                  </label>
+               </div>
+            </div>
+            <div>
+               <button className="signin-btn">SIGN IN</button>
+            </div>
          </div>
       </div>
    );
